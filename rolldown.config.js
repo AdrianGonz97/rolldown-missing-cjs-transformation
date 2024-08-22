31bb4f741ba1bb8e67b2b330689bbbb7cc669dcf
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-08-22 16:32:51 -0400

    tweak

--- rolldown.config.js
+++ rolldown.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "rolldown";
 
 export default defineConfig({
-	input: "index.ts",
+	input: "entry.js",
 	output: {
 		dir: "dist-rolldown",
 		format: "esm",
+		entryFileNames: "index.js",
 	},
 	platform: "node",
 });
--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from "rollup";
 import commonjs from "@rollup/plugin-commonjs";
 
 export default defineConfig({
-	input: "index.ts",
+	input: "entry.js",
 	output: {
 		dir: "dist-rollup",
 		format: "esm",
+		entryFileNames: "index.js",
 	},
 	platform: "node",
 	plugins: [commonjs()],
